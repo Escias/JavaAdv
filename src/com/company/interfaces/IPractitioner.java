96ffdcc23c06268eaf903786f78cbcff12976fa6
Escias <ljimmy98@hotmail.fr>
2021-01-13 09:39:40 +0100

    first interface

---
+++ src/com/company/interfaces/IPractitioner.java
@@ -0,0 +1,12 @@
+package com.company.interfaces;
+
+public interface IPractitioner {
+
+    void matricule();
+    void lastname();
+    void firstname();
+    void speciality();
+    void grade();
+    void hospital();
+    void hourAndPricing();
+}
