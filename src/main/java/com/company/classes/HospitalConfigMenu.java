5caf4d30cce2fd41057272c5526d667dfe250ec9
Escias <ljimmy98@hotmail.fr>
2021-01-14 15:53:52 +0100

    remove uneccessary declaration

--- src/main/java/com/company/classes/HospitalConfigMenu.java
+++ src/main/java/com/company/classes/HospitalConfigMenu.java
@@ -15,7 +15,7 @@ import java.util.Scanner;
 public class HospitalConfigMenu {
     public static List<Doctor> host = new ArrayList();
 
-    public void Config() throws IOException {
+    public void Config(){
         System.out.println("Config your hospital\n");
         Scanner f = new Scanner(System.in);
         int floor;
@@ -98,34 +98,4 @@ public class HospitalConfigMenu {
         file.write(hospitalsList.toString());
         file.flush();
     }
-
-    /*public void SaveHospital(int floor, int room) throws IOException {
-        JSONArray hospitalList = new JSONArray();
-        List<String> chambers = new ArrayList<>();
-        String floorNumber;
-        String chamber;
-        for (int j=0; j <= floor; j++){
-            for (int i=1; i<=room; i++){
-                if (j==0){
-                    String zero = String.valueOf(i);
-                    chambers.add(zero);
-                }else if (i < 100 && i >= 10){
-                    String zero = String.format("%01d", i);
-                    chamber = j+zero;
-                    chambers.add(chamber);
-                }else if (i < 10){
-                    String zero = String.format("%02d", i);
-                    chamber = j+zero;
-                    chambers.add(chamber);
-                }
-            }
-            JSONObject hospitalDetails=new JSONObject();
-            floorNumber = String.valueOf(j);
-            hospitalDetails.put(floorNumber, chambers);
-            hospitalList.put(hospitalDetails);
-        }
-        FileWriter file = new FileWriter("hospital.json");
-        file.write(hospitalList.toString());
-        file.flush();
-    }*/
 }
